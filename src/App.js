c70a3a8bc73e61b722a7fec0cfb4a6b0060120b5
Veter v Golove <koshelevworkmail@gmail.com>
2022-09-23 20:01:39 -0400

    FPV camera and cursor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 import { Ground } from "./components/Ground";
 import { Player } from "./components/Player";
+import { FPV } from "./components/FPV";
+import { cursor } from "./images/images";
 
 function App() {
     return (
@@ -10,13 +12,17 @@ function App() {
             <Canvas>
                 <Sky sunPosition={[100, 100, 20]} />
                 <ambientLight intensity={0.5} />
-
+                {/* CAMERA */}
+                <FPV />
                 {/* GROUND */}
                 <Physics>
                     <Player />
                     <Ground />
                 </Physics>
             </Canvas>
+            <div className="absolute centered">
+                <img src={cursor} alt="cursor" />
+            </div>
         </>
     );
 }
--- src/images/images.js
+++ src/images/images.js
@@ -3,5 +3,6 @@ import grassImg from "./grass.jpg";
 import glassImg from "./glass.png";
 import logImg from "./log.jpg";
 import woodImg from "./wood.png";
+import cursor from "./cursor.png";
 
-export { dirtImg, grassImg, glassImg, woodImg, logImg };
+export { dirtImg, grassImg, glassImg, woodImg, logImg, cursor };
